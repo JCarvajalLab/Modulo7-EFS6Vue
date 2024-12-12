b4c296385ab7241923c531d22c8ddacae70f352c
JCarvajalLab <carvajal.jordan94@gmail.com>
2024-12-12 16:51:49 -0300

    se crean las view y los componentes, se actualizan las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HeroesView from '../views/HeroesView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -8,12 +10,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/heroes',
+    name: 'heroes',
+    component: HeroesView
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'notFound',
+    component: NotFound
   }
 ]
 
